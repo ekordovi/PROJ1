797e9f00134534d11dd6642248f2422d49aa04e0
Evan_Kordovi <ekordovi@gmail.com>
2024-02-29 18:42:07 -0800

    commit of HS_chrom19_GRCh38.p14.R turning the dna sequence inta AA seq. used DNAString.

--- HS_chrom19_GRCh38.p14.R
+++ HS_chrom19_GRCh38.p14.R
@@ -4,10 +4,19 @@ install.packages("tidyverse")
 library(Biostrings)
 library(ggplot2)
 library(seqinr)
+library(XNAString)
+library(ape)
+BiocManager::install("XNAString")
 fasta_data <- read.fasta("sequence.fasta", as.string = TRUE)
 print(head(fasta_data))
 str(fasta_data)
 seq <- paste(fasta_data, collapse = "")
 seq
 dnastring = DNAString(seq)
-dnastring
+class(dnastring)
+
+protein_seq <- Biostrings::translate(dnastring)
+protein_seq
+alphabetFrequency(dnastring, baseOnly=TRUE, as.prob=TRUE)
+
+
