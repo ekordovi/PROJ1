d735a9732fdf00a859efd21799b4df00e6ff02c4
Evan_Kordovi <ekordovi@gmail.com>
2024-02-29 17:15:59 -0800

    2nd commit of Retrieve_seq.R fixing the packages needed.

--- Retrieve_seq.R
+++ Retrieve_seq.R
@@ -1,23 +1,38 @@
-# Install necessary packages (if not already installed)
+# Install necessary packages 
 install.packages("rentrez") # For accesing NCBI
 install.packages("ape")      # For phylogenetic tree manipulation and visualization
 install.packages("seqinr")   # For sequence manipulation
+# Bioconductor package - DECIPHER
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
+BiocManager::install("DECIPHER")
+BiocManager::install("Biostrings")
 # Load necessary libraries
 library(rentrez)
 library(ape)
 library(seqinr)
-
+library(Biostrings)
+library(DECIPHER)
 # 1. Retrieve Sequences
 query <- "Phaeobacter piscinae"
 search_results <- entrez_search(db = "nucleotide", term = query, retmax = 100)  # Adjust retmax as needed
 ids <- search_results$ids
 sequences <- entrez_fetch(db = "nucleotide", id = ids, rettype = "fasta")
 
-# 2. Sequence Alignment (optional)
-# Perform sequence alignment using a tool like Clustal Omega or MAFFT
-# You can use the Bioconductor package DECIPHER or other alignment tools for this purpose
+sequences_list <- scan(text = sequences, what = character(), sep = ">")
 
+# Remove any empty strings
+sequences_list <- sequences_list[sequences_list != ""]
+
+# Read the sequences into R
+dna <- readDNAStringSet(sequences_list)
+
+
+BiocManager::valid()
+R.version
+# 2. Sequence Alignment (optional)
+AlignSeqs(dna)
 # 3. Phylogenetic Inference
 # Once you have aligned sequences, you can use phylogenetic inference methods
 # For example, you can use the 'phangorn' package for maximum likelihood phylogenetic inference
