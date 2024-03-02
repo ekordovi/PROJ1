653be9c691816a9af000efaef29776e683f3c027
Evan_Kordovi <ekordovi@gmail.com>
2024-03-02 00:05:51 -0800

    initial commit of my semi-parsed my_transcripts.fasta and commit of Retrieve_seq.R (made my_transcripts.fasta)

--- Retrieve_seq.R
+++ Retrieve_seq.R
@@ -18,21 +18,44 @@ library(DECIPHER)
 query <- "Phaeobacter piscinae"
 search_results <- entrez_search(db = "nucleotide", term = query, retmax = 100)  # Adjust retmax as needed
 ids <- search_results$ids
-sequences <- entrez_fetch(db = "nucleotide", id = ids, rettype = "fasta")
+sequences <- entrez_fetch(db = "nuccore", id = ids, rettype = "fasta")
+cat(strwrap(substr(sequences, 1, 500)), sep="\n")
+
+write(sequences, file="my_transcripts.fasta")
 
 sequences_list <- scan(text = sequences, what = character(), sep = ">")
-
+sequence_set <- readDNAStringSet(sequences, format = "fasta")
+print(head(sequence_set))
 # Remove any empty strings
 sequences_list <- sequences_list[sequences_list != ""]
+print(head(sequences_list))
+length(sequences_list)
+class(sequences_list)
 
-# Read the sequences into R
-dna <- readDNAStringSet(sequences_list)
+split_sequences <- strsplit(sequences_list, " ")
+head(split_sequences)
+vclass(split_sequences)
+# split_sequences is a list of characters
+# i want to turn these characters into Bstrings
+# Initialize an empty list to store BString objects
+list_Bstrings <- list()
+
+# Convert each character vector to a BString and append to the list
+for (i in split_sequences) {
+  if (length(i) == 1 && !is.na(i)) {
+    list_Bstrings <- c(list_Bstrings, BString(i))
+  } else {
+    # Handle cases where i is not a single character vector
+    # Print a message or take appropriate action
+    print(paste("Skipping invalid sequence:", i))
+  }
+}
 
 
-BiocManager::valid()
-R.version
+dna_set <- DNAStringSet(split_sequences)
+
 # 2. Sequence Alignment (optional)
-AlignSeqs(dna)
+AlignSeqs(split_sequences[1], split_sequences[2])
 # 3. Phylogenetic Inference
 # Once you have aligned sequences, you can use phylogenetic inference methods
 # For example, you can use the 'phangorn' package for maximum likelihood phylogenetic inference
