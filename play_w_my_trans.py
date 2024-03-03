fae402e47734904d2fcf7f8e097498ab548a67df
Evan_Kordovi <ekordovi@gmail.com>
2024-03-03 00:15:11 -0800

    commit added seq's to a sqlite3 db

--- play_w_my_trans.py
+++ play_w_my_trans.py
@@ -1,12 +1,31 @@
 from Bio import SeqIO
+from BioSQL import BioSeqDatabase
+import sqlite3
+import psycopg2
 
+
+# server = BioSeqDatabase.open_database(driver = "MySQLdb", user = "chapmanb",
+#                  passwd = "biopython", host = "localhost", db = "bioseqdb")
+
+conn = sqlite3.connect('PROJ1_BioSQL.sql.db')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS Sequences (id TEXT, description TEXT UNIQUE, seq Text)''')
 fasta_sequences = SeqIO.parse(open("my_transcripts.fasta"),'fasta')
 bling = 0
+
+records = list()
 for record in fasta_sequences:
     bling += 1
-    print(record.id)
+    cur.execute("INSERT OR IGNORE INTO Sequences (id, description, seq) VALUES (?, ?, ?)", 
+                (record.id, record.description, str(record.seq)))
+    records.append(record.seq)
     if bling == 1:
         print(record, record.id)
+print("Sequences parsed: ", bling)
+
+conn.commit()
+
+conn.close()
 
 
-records = list(fasta_sequences)
