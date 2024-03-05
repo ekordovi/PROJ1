a75e6a880126d9b46deb982d3679ff01ec820921
Evan_Kordovi <ekordovi@gmail.com>
2024-03-04 20:42:42 -0800

    Commit putting translated AA_seqs into DB

--- Translated_seqs.R
+++ Translated_seqs.R
@@ -16,6 +16,14 @@ for (i in 1:nrow(results)) {
   protein_seqs[[i]] <- as.character(protein_seq)
 }
 
+
+# Loop through the AA sequences and insert them into the database
+for (seq in protein_seqs) {
+  dbExecute(conn, "INSERT INTO AA_Sequences (AA_seq) VALUES (?)", params = list(seq))
+}
+
+# Commit changes and close the connection
+
 print(protein_seqs[95])
 
 
