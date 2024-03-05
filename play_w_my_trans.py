4f42422ccb37113d32a82195ef3550b825e1eb7d
Evan_Kordovi <ekordovi@gmail.com>
2024-03-04 16:10:27 -0800

    identified motifs in each sequence.

--- play_w_my_trans.py
+++ play_w_my_trans.py
@@ -1,3 +1,4 @@
+import re
 from Bio import SeqIO
 import sqlite3
 
@@ -11,6 +12,8 @@ cur = conn.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS Sequences (id TEXT, description TEXT UNIQUE, seq Text)''')
 fasta_sequences = SeqIO.parse(open("my_transcripts.fasta"),'fasta')
 bling = 0
+first_record = next(fasta_sequences)
+print(first_record)
 
 records = []
 for record in fasta_sequences:
@@ -22,11 +25,9 @@ for record in fasta_sequences:
         print(record, record.id)
 print("Sequences parsed: ", len(records))
 
+
 table_seq = cur.execute("SELECT (seq) FROM Sequences")
 all_seqs = [record[0] for record in cur.fetchall()]
-
-
-
 
 seq_lengths = [len(seq) for seq in all_seqs]
 
@@ -35,6 +36,24 @@ print("min length:", min(seq_lengths))
 print("Maximum length:", max(seq_lengths))
 print("Mean length:", sum(seq_lengths) / len(seq_lengths))
 
+# defining motif pattern
+motif_pattern = re.compile(r"AT[A|T]G")
+
+# motif search
+motif_positions_dict = {}
+motif_positions_dict = {}
+try:
+    for record, seq in zip(records, all_seqs):
+        seq_id = record.id
+        matches = motif_pattern.finditer(seq)
+        motif_positions = [match.start() for match in matches]
+        motif_positions_dict[seq_id] = motif_positions
+except Exception as e:
+    print("An error occurred while processing motif positions:", e)
+
+# printing motif positions for each seq. 
+#for seq_id, motif_positions in motif_positions_dict.items():
+#    print(f"Motif matches in sequence {seq_id}: {motif_positions}")
 
 conn.commit()
 conn.close()
